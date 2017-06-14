01aca14615af0fdc534ec0cd42271188a777e433
diol <diolll25@gmail.com>
2017-06-14 21:04:49 +0900

    css

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -16,17 +16,26 @@ class TodosController < ApplicationController
   end
 
   def stack
-    @todo = Todo.new(:task => Todo.first.task,:count => Todo.first.count)
-    @todo.add_count
-    #@todo.update_attributes = { "count = (@todo.count+1) "}
-    if @todo.save
-      @todo = Todo.first
-      @todo.destroy
+    if Todo.exists? == false
       redirect_to root_path
     else
-      redirect_to root_path
+      @todo = Todo.new(:task => Todo.first.task,:count => Todo.first.count)
+      @todo.add_count
+      if @todo.count >= 3
+        @todo = Todo.first
+        @todo.destroy
+        redirect_to root_path
+      else
+        #@todo.update_attributes = { "count = (@todo.count+1) "}
+        if @todo.save
+          @todo = Todo.first
+          @todo.destroy
+          redirect_to root_path
+        else
+          redirect_to root_path
+        end
+      end
     end
-    
   end
 
   def destroy
