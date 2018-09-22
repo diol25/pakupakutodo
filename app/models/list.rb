540be9953bbea9b2df72da5c257572ca6ab7cbaa
KotaSakurai <sakurai.kota@mwed.co.jp>
2018-09-22 23:11:06 +0900

    use enum

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,11 +1,7 @@
 class List < ApplicationRecord
   has_many :tasks
 
-  def todos
-    tasks.select{ |task| task.status == 0 }
-  end
-
   def count
-    self.todos.count
+    self.tasks.todo.count
   end
 end
